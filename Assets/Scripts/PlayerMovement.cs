71eec5339a3449043cab5e03f844286e63a418af
Drago103 <benjamin.see3@gmail.com>
2026-03-29 21:37:47 +1100

    FirstPersonPerspectiveCamera
    installed cinemachine camera package, added first person perspective and making the camera rotate when the player collides into the wall.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -6,9 +7,13 @@ public class PlayerMovement : MonoBehaviour
     private PlayerControls controls;
     private Vector2 moveInput;
 
+    [SerializeField] Transform headTarget;
+
     [SerializeField] float MoveSpeed = 5;
 
     [SerializeField] Rigidbody rb;
+
+    private bool IsRotating = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -31,19 +36,46 @@ public class PlayerMovement : MonoBehaviour
 
     void MovePlayer()
     {
-        Vector3 Move = new Vector3(moveInput.x, 0f, 1f) * MoveSpeed;
-        rb.linearVelocity = new Vector3(Move.x, rb.linearVelocity.y, Move.z);
+       Vector3 Move = new Vector3(moveInput.x, 0f, 1f) * MoveSpeed;
+       rb.linearVelocity = new Vector3(Move.x, rb.linearVelocity.y, Move.z);
     }
+
 
     void OnCollisionEnter(Collision other)
     {
         if (other.collider.CompareTag("Wall"))
         {
             Debug.Log("Hit a Wall");
+
             transform.Rotate(0f,180f,0f);
+
+            if (!IsRotating)
+            {
+                StartCoroutine(SmoothRotate(0.35f));
+            }
+
             rb.linearVelocity = -rb.linearVelocity *2;
             MoveSpeed = -MoveSpeed;
         }
+    }
+
+    IEnumerator SmoothRotate(float duration)
+    {
+        IsRotating = true;
+
+        Quaternion startRot = headTarget.rotation;
+        Quaternion endRot = headTarget.rotation * Quaternion.Euler(0f,180f,0f);
+
+        float time = 0f;
+
+        while (time < 1f)
+        {
+            time += Time.deltaTime / duration;
+            headTarget.rotation = Quaternion.Slerp(startRot, endRot, time);
+            yield return null;
+        }
+
+        IsRotating = false;
     }
 
     // Update is called once per frame
