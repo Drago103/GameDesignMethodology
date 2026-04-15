7c7d1e40b68438ac5937eaab45015cf5d85b13e7
Wuhaiyu <jarrodjamesngo@gmail.com>
2026-04-15 19:48:30 +1000

    deathmechanic
    gameover!

---
+++ Assets/Scripts/gameover.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameover : MonoBehaviour {
+    public string loadscene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(loadscene);
+        }
+    }
+}
